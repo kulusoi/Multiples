6caf59da9cb2e3278f620c7a32be4eda69c28193
kulusoi <jason.wang2@emory.edu>
2016-06-19 21:36:17 +0800

    My first App

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var startPoint = 0;
+    var sum = 0;
+    var count = 0;
+    
+    @IBOutlet weak var multipleLbl: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var inputTxt: UITextField!
+    
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    @IBAction func startTheGame(sender: UIButton!){
+        
+        if inputTxt.text != nil && inputTxt.text != ""{
+            multipleLbl.hidden = true;
+            playBtn.hidden = true;
+            inputTxt.hidden = true;
+            
+            addBtn.hidden = false;
+            resultLbl.hidden = false;
+            
+            startPoint = Int(inputTxt.text!)!;
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addingSum(sender: UIButton){
+        if (count < 5){
+            resultLbl.text = "\(sum) + \(startPoint) = \(sum + startPoint)";
+            sum += startPoint;
+            count += 1;
+        }else{
+            multipleLbl.hidden = false;
+            playBtn.hidden = false;
+            inputTxt.hidden = false;
+            
+            addBtn.hidden = true;
+            resultLbl.hidden = true;
+            
+            sum = 0;
+            startPoint = 0;
+            count = 0;
+            resultLbl.text = "Press Added to add!";
+            inputTxt.text = "";
+        }
+        
     }
-
-
 }
 
